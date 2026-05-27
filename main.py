0b30dbcdc1126ba6eb7e85ef5d66d0df5a9de5b3
Tyler Jayden Wu <tyler_wu@berkeley.edu>
2026-05-27 06:55:08 +0000

    Updated Data Processing and Backend

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 # NBA Live Win Predictor, app startup, and routing
 
-import websocket_client
-
 import jsonify
 
 from flask import Flask, render_template, request
 import torch
 from train.py import WinPredictor, data
+from stats.py import pull_live_scoreboard, pull_live_play_by_play
 
 # load model weights so only need to pull play-by-play data and box score data in main.py to make predictions and inference at runtime in live games, no need to retrain model every time
 model = WinPredictor()
@@ -26,12 +25,16 @@ def predict():
 
     return jsonify({'win_pct': prob.item()})
 
-cache = {}
+cache = {} # store last live update data in cache in case api endpoint hasn't updated 
 
 @app.route('/live-data')
 def live_data():
-    websocket_client.start_websocket()
-    return jsonify(cache)
+    scoreboard = pull_live_scoreboard()
+    play_by_play = pull_live_play_by_play()
+
+    live_data = {"win_pct": scoreboard, "time_left": play_by_play['clock'], "quarter": play_by_play['period'], homeTeam}
+
+    return jsonify(live_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- stats.py
+++ stats.py
@@ -1,23 +1,21 @@
 # pull play-by-play data for training win prediction model on live games
-from nba_api.stats.endpoints import playbyplayv3
 from nba_api.stats.endpoints import boxscoretraditionalv3
-from nba_api.live.nba.endpoints import scoreboard
+from nba_api.live.nba.endpoints import scoreboard, playbyplay
 import pandas as pd
 
 
-def pull_live_data():
+def pull_live_scoreboard():
     try:
         scoreboard_data = scoreboard.ScoreBoard().games.get_dict()['scoreboard']
         return pd.DataFrame(scoreboard_data)
     except Exception as e:
-        print(f"Error pulling live data: {e}")
+        print(f"Error pulling live scoreboard: {e}")
         return pd.DataFrame()  # Return an empty DataFrame in case of error
 
-def pull_play_by_play_data(game_id):
-    play_by_play_v3 = playbyplayv3.PlayByPlayV3(game_id=game_id)
-    return play_by_play_v3
-
-# export data to csv for training
-
-def export_data_to_csv(data, filename):
-    data.to_csv(filename, index=False)
+def pull_live_play_by_play():
+    try:
+        play_by_play = playbyplay.PlayByPlay().get_dict()['game']['actions'] # could also return game id here if needed 
+        return pd.DataFrame(play_by_play)
+    except Exception as e:
+        print(f"Error pulling live play bby play: {e}")
+        return pd.DataFrame()
